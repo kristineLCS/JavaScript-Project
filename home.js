e4823044ee19c04cf51fd77c1a64bb3282c045da
Kristine Segovia <kristinesegovia23@gmail.com>
2024-08-03 23:15:53 +0100

    added alert to sign up form but need to delete password input as no newsletter requires passwords

--- home.js
+++ home.js
@@ -1,16 +1,27 @@
-const login = document.getElementById('login');
+const signup = document.getElementById('signup');
 const email = document.getElementById('email-input');
 const password = document.getElementById('password-input');
-const loginButton = document.getElementById('loginbutton');
+const signupButton = document.getElementById('signupbutton');
+const checkbox = document.getElementById('passcheckbox');
 
 
-loginButton.addEventListener('click', loginBtn);
+signupButton.addEventListener('click', signupBtn);
 
-function loginBtn() {
+function signupBtn() {
     if (email.value === ''  || password.value  === '') {
         alert('Please enter email and password');
     } else {
-        alert('You have successfully logged in!');
+        alert('You have successfully signed up!');
     }
     
+}
+
+checkbox.addEventListener('click', hidePassword);
+
+function hidePassword() {
+    if (password.type === "password") {
+        password.type = "text";
+    } else {
+        password.type = 'password';
+    }
 }
